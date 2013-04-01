dcfcfdfe6b49960b34a49842ffa2e626040c476e
Robert Truesdale <robert.truesdale@rackspace.com>
2013-04-01 17:46:26 -0500

    This is for Challenge 1 Create 3 512 servers and list user credentials and ip address of server

---
+++ challenge1.php
@@ -0,0 +1,84 @@
+<?php
+// (c)2012 Rackspace Hosting
+// See COPYING for licensing information
+
+namespace OpenCloud;
+
+require_once('rackspace.php');
+require_once('compute.php');
+
+#define('AUTHURL', RACKSPACE_US);
+#define('USERNAME', $_ENV['OS_USERNAME']);
+#define('TENANT', $_ENV['OS_TENANT_NAME']);
+#define('APIKEY', $_ENV['NOVA_API_KEY']);
+
+define('AUTHURL', 'https://identity.api.rackspacecloud.com/v2.0/');
+define('USERNAME', 'robe4339');
+define('TENANT', '683020');
+define('APIKEY', '34f3feecc867c5fc41371edea5b31c58');
+
+/**
+ * numbers each step
+ */
+function step($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    global $STEPCOUNTER;
+    printf("\nStep %d. %s\n", ++$STEPCOUNTER, sprintf($msg,$p1,$p2,$p3));
+}
+
+function info($msg,$p1=NULL,$p2=NULL,$p3=NULL) {
+    printf("  %s\n", sprintf($msg,$p1,$p2,$p3));
+}
+
+function dot($server) {
+	printf("%s %3d%%\n", $server->status, $server->progress);
+}
+
+define('TIMEFORMAT', 'r');
+
+
+// establish our credentials
+step('Authenticate');
+$connection = new Rackspace(AUTHURL,
+	array( 'username' => USERNAME,
+		   'apiKey' => APIKEY ));
+
+step('Connect to Cloud Servers');
+// now, connect to the compute service
+$compute = $connection->Compute('cloudServersOpenStack', 'DFW');
+
+/**
+ * Let's build a server. We want to have an OS of CentOS 6.0 or higher and a
+ * Flavor with at least 512 of RAM
+ */
+
+step('Create 3 servers ');
+$list = $compute->ImageList(TRUE, array('name'=>'CentOS 6.3'));
+$image = $list->First();
+$flavor = $compute->Flavor(2); // 512MB
+
+// Name the servers
+$servername = "Web";
+
+//We're going to make 3 servers
+$amountofservers = '3';
+
+$i=0;
+for($i=1; $i <= $amountofservers; $i++){
+// let's create the servers
+info("Creating Server $servername$i");
+$server = $compute->Server();
+$server->Create(array(
+		'name' => "$servername$i",
+		'image' => $image,
+		'flavor' => $flavor));
+print("requested, now waiting...\n");
+#print("ID=".$server->id."...\n");
+
+$server->WaitFor("ACTIVE", 900, 'OpenCloud\dot');
+printf("ID is %s, root password is %s, IP Adress is %s\n", $server->id, $server->adminPass, $server->ip());
+}
+
+print("done\n");
+exit(0);
+
+
